b7238bd1e0983369994abdc9415599468edac17f
MariannaSarkisian <spn.mjj@gmail.com>
2022-10-08 23:49:37 +0300

    1z

---
+++ 1z/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти кубы чисел от 1 до N
+Console.Write("enter number: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int count = 1;
+int res=0;
+while (count <= number)
+{
+    //*res = Math.Pow(count, 3);
+    res = count*count*count;
+    Console.WriteLine($"{count}^3={res}");
+    count++;
+}
