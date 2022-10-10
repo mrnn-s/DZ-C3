c8d99fce8af98bd527dc277245b76a489994a093
MariannaSarkisian <spn.mjj@gmail.com>
2022-10-10 14:30:31 +0300

    3

--- 3z/Program.cs
+++ 3z/Program.cs
@@ -1,1 +1,22 @@
 ﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.Write("enter number: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int result=1;
+for (int i=1;i<=number;i++)
+{
+   result=result*i;
+   Console.WriteLine(result);
+}
+
+
+
+
+// Console.Write("Vvedite числo:");
+// int Chislo = int.Parse(Console.ReadLine() ?? "0");
+// int sch = 1;
+// for (int i=1; i<=Chislo;i++)
+// {
+// sch = sch * i;
+// Console.WriteLine(sch);
+// }
