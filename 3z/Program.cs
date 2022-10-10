19e92f58f73fb940d8e53f5e316c543b23e08e3c
MariannaSarkisian <spn.mjj@gmail.com>
2022-10-10 14:47:06 +0300

    4

--- 3z/Program.cs
+++ 3z/Program.cs
@@ -12,11 +12,3 @@ for (int i=1;i<=number;i++)
 
 
 
-// Console.Write("Vvedite числo:");
-// int Chislo = int.Parse(Console.ReadLine() ?? "0");
-// int sch = 1;
-// for (int i=1; i<=Chislo;i++)
-// {
-// sch = sch * i;
-// Console.WriteLine(sch);
-// }
--- 4z/Program.cs
+++ 4z/Program.cs
@@ -1,1 +1,11 @@
 ﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.Write("enter number: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int result=1;
+for(int i=1;i<=number;i++)
+{
+    result=i*i*i;
+if(result%2==0)
+    Console.WriteLine(result);
+
+}
